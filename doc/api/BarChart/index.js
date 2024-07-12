08ea8d06c23156311b2c76b95365ba774c44eef0
KAI0491 <kai.liu0491@gmail.com>
2024-07-12 01:23:42 -0700

    docs: remove invalid attributes and related demos from the line-chart api section

--- doc/api/BarChart/index.js
+++ doc/api/BarChart/index.js
@@ -8,15 +8,12 @@ import { xAxisMd, xAxis } from '../common/xAxis'
 import { yAxisMd, yAxis } from '../common/yAxis'
 import { dataZoomMd, dataZoom } from '../common/dataZoom'
 import { eventMd, event } from '../common/event'
-import { linearGradientMd, linearGradient } from '../common/linearGradient'
 import dataMd from './data.md?raw'
 import itemStyleMd from './itemStyle.md?raw'
 import typeMd from './type.md?raw'
 import directionMd from './direction.md?raw'
 import labelMd from './label.md?raw'
 import lineDataNameMd from './lineDataName.md?raw'
-import stepGradientMd from './stepGradient.md?raw'
-import axisPointerMd from './axisPointer.md?raw'
 import topTipHtmlMd from './topTipHtml.md?raw'
 import markLineMd from './markLine.md?raw'
 
@@ -31,12 +28,9 @@ const data = {
         tipHtml,
         tipHtmlStyle,
         dataZoom,
-        linearGradient,
         event,
         ['data', '图表数据(必填)', 'array', '无'],
         ['type', '配置柱状图类型', 'string', 'bar'],
-        ['stepGradient', '柱体分段渐变', 'array', '无'],
-        ['axisPointer', '坐标指示器样式（数据背景区域样式）', 'object', 'rgba(25,25,25,0.08)'],
         ['topTipHtml', '顶部位置悬浮提示框', 'function', '默认不显示'],
         ['itemStyle', '柱体样式', 'object', '{barWidth:8,barGap:\'60 %\'}'],
         ['direction', '柱体方向', 'string', 'vertical'],
@@ -54,12 +48,9 @@ const data = {
         tipHtmlMd,
         tipHtmlStyleMd,
         dataZoomMd,
-        linearGradientMd,
         eventMd,
         dataMd,
         typeMd,
-        stepGradientMd,
-        axisPointerMd,
         topTipHtmlMd,
         itemStyleMd,
         directionMd,
