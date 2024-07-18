2396c3615c1b1eadd53e000957e6a16159a88f98
wenbin-eng <617780572@qq.com>
2024-07-17 20:36:46 -0700

    feat:(pie-chart): added the ability to set specific colors (#26)

--- src/components/PieChart/index.js
+++ src/components/PieChart/index.js
@@ -38,6 +38,8 @@ class PieChart {
     const position = iChartOption.position || iChartOption.chartPosition;
     // 处理series数据
     this.baseOption.series = handleSeries(type, iChartOption, chartInstance, position);
+    // 针对给定的color值，需要进行特殊处理
+    this.baseOption.color = iChartOption.color;
     // 针对多重圆环图表需求，图表需要进行特殊处理
     handleMulti(type, this.baseOption, iChartOption.legend, iChartOption.data);
     // 是否关闭hover态的效果，默认为false
