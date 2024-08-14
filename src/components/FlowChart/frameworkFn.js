ec788ae8d4c6dddc2488514115ba2089faf311db
Guanmingjiang <15298397118@163.com>
2024-08-14 16:27:22 -0700

    fix(FlowChart):Annotation Framework Code

--- src/components/FlowChart/frameworkFn.js
+++ src/components/FlowChart/frameworkFn.js
@@ -1,38 +1,38 @@
-// Vue 框架依赖配置
-import { createApp, h } from 'vue';
+// // Vue 框架依赖配置
+// import { createApp, h } from 'vue';
 
-// React 框架依赖配置
-import ReactDOMServer from 'react-dom/server';
+// // React 框架依赖配置
+// import ReactDOMServer from 'react-dom/server';
 
-// Angular 框架依赖配置
-import { ViewContainerRef } from '@angular/core';
+// // Angular 框架依赖配置
+// import { ViewContainerRef } from '@angular/core';
 
-export function createVueApp(options) {
-  if (!createApp) {
-    throw new Error('Vue is not installed. Please install Vue to use createApp.');
-  }
-  return createApp(options);
-}
+// export function createVueApp(options) {
+//   if (!createApp) {
+//     throw new Error('Vue is not installed. Please install Vue to use createApp.');
+//   }
+//   return createApp(options);
+// }
 
-export function createElement(tag, props, children) {
-  if (!h) {
-    throw new Error('Vue is not installed. Please install Vue to use the createElement function.');
-  }
-  return h(tag, props, children);
-}
+// export function createElement(tag, props, children) {
+//   if (!h) {
+//     throw new Error('Vue is not installed. Please install Vue to use the createElement function.');
+//   }
+//   return h(tag, props, children);
+// }
 
-export function renderToString(Component) {
-  const { renderToString } = ReactDOMServer;
+// export function renderToString(Component) {
+//   const { renderToString } = ReactDOMServer;
 
-  if (!renderToString) {
-    throw new Error('react-dom is not installed. Please install react-dom to use the renderToString function.');
-  }
-  return renderToString(Component);
-}
+//   if (!renderToString) {
+//     throw new Error('react-dom is not installed. Please install react-dom to use the renderToString function.');
+//   }
+//   return renderToString(Component);
+// }
 
-export function AngularViewContainerRef() {
-  if (!ViewContainerRef) {
-    throw new Error('@angular/core is not installed. Please install @angular/core to use the renderToString function.');
-  }
-  return ViewContainerRef;
-}
+// export function AngularViewContainerRef() {
+//   if (!ViewContainerRef) {
+//     throw new Error('@angular/core is not installed. Please install @angular/core to use the renderToString function.');
+//   }
+//   return ViewContainerRef;
+// }
