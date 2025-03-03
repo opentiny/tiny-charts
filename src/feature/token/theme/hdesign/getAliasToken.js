6f77a683758355187b7818bbcf43f9e372a0587a
KAI0491 <kai.liu0491@gmail.com>
2025-03-03 03:40:40 -0800

    feat(ProcessChart): barWidth and tooltip content adaptation themes

--- src/feature/token/theme/hdesign/getAliasToken.js
+++ src/feature/token/theme/hdesign/getAliasToken.js
@@ -33,7 +33,6 @@ function getAliasToken(globalToken, light = true) {
     spaceNone,
     space4x,
     size6x,
-    size5x,
     size05x,
     borderRadiusLg,
   } = globalToken;
@@ -206,6 +205,9 @@ function getAliasToken(globalToken, light = true) {
     paddingSM: spaceBase,
     padding: space2x,
     paddingLG: space4x,
+    tipItemGap:size2x,
+    tipIconGap:size2x,
+    tipValueGap:size4x,
     // -----------------------------------------------------------------边框------------------------------------------------------------------------------
     // zoom数据区域边框
     zoomDataAreaBorderWidth: borderBase,
@@ -228,8 +230,8 @@ function getAliasToken(globalToken, light = true) {
     symbolSizeSM: sizeBase,
     // 柱条的宽度
     barWidth: size4x,
-    // 堆叠进度图宽度 20
-    barWidthLG: size5x,
+    // 堆叠进度图宽度
+    barWidthSecondary: size4x,
     // 图例单元尺寸
     legendItemSize: size05x,
     // 图例圆形单元尺寸
