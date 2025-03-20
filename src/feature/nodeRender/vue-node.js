b0c4815996e4b65c9b0ce027a62c2208c2078811
ader-h <1059803085@qq.com>
2025-03-20 14:48:30 -0700

    fix(vueNode): add isVue judging condition

--- src/feature/nodeRender/vue-node.js
+++ src/feature/nodeRender/vue-node.js
@@ -65,7 +65,7 @@ function isVueComponent(component) {
   if (typeof component === 'function' && component.prototype && component.prototype.$options) {
     return true;
   }
-  if (typeof component === 'object' && (component.render || (component.type?.render && component.type?.__scopeId))) {
+  if (typeof component === 'object' && (component.render || (component.type?.render && component.type?.__scopeId) || component.__scopeId)) {
     return true;
   }
   return false;
