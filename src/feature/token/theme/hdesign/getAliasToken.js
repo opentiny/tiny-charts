5df36ad8935ff5d58f8faf88ec2287c727c8e0ef
KAI0491 <kai.liu0491@gmail.com>
2025-04-07 02:15:57 -0700

    feat(token): token  add colorBgActive and colorBgControl

--- src/feature/token/theme/hdesign/getAliasToken.js
+++ src/feature/token/theme/hdesign/getAliasToken.js
@@ -45,6 +45,7 @@ function getAliasToken(globalToken, sceneToken) {
     colorBgPlaceholder,
     colorBgHover,
     colorBgHandle,
+    colorBgActive,
     colorTextPrimary,
     colorTextSecondary,
     colorTextPlaceholder,
@@ -78,6 +79,8 @@ function getAliasToken(globalToken, sceneToken) {
     colorBgContainerSecondary: colorBgSecondary,
     // 悬浮背景
     colorBgContainerHover: colorBgHover,
+    // 仪表盘中心文本卡片
+    colorBgControl: colorBgActive,
     // 标题颜色
     colorTitle: colorTextPrimary,
     // 副标题颜色
