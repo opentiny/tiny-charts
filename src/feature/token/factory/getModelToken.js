d653a558376a436737d57b892f7465dc5742e610
lingdan33 <1326571305@qq.com>
2025-04-17 19:56:02 +0800

    fix(Legend): modify padding to align legend

--- src/feature/token/factory/getModelToken.js
+++ src/feature/token/factory/getModelToken.js
@@ -134,7 +134,7 @@ function getModelToken(aliasToken) {
     // 图例富文本数值字号
     legendTextValueFontSize: textFontSize,
     // 图例文本的padding
-    legendTextPadding: [containerGap, 0, 0, 0],
+    legendTextPadding: [containerGap - 1, 0, 0, 0],
     // 图例每项之间的间隔
     legendItemGap: legendSpace,
     // 圆形图例时每项的宽度
@@ -413,7 +413,7 @@ function getModelToken(aliasToken) {
     // 状态切换的动画缓动
     stateAnimationEasing: 'quinticInOut',
     // 全局颜色透明度
-    globalColorAlpha:colorAlpha
+    globalColorAlpha: colorAlpha
   };
 }
 
