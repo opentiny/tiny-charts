fc6963597c030d23b4e4a779996ebf4b9a3ac080
ader-h <1059803085@qq.com>
2025-04-18 11:13:24 -0700

    fix(ldata): The chart is abnormal when the first item of data does not exist

--- src/option/config/legend/ldata.js
+++ src/option/config/legend/ldata.js
@@ -13,15 +13,18 @@
  * 从数据中拿出legend-data
  */
 function ldata(data, xAxisKey) {
-    const legendData = [];
+    let legendData = [];
     if (data.length > 0) {
-        const temp = data[0];
-        for (const key in temp) {
-            if (key !== xAxisKey) {
-                legendData.push(key);
+        data.forEach(temp => {
+            for (const key in temp) {
+                if (key !== xAxisKey) {
+                    legendData.push(key);
+                }
             }
-        }
+        });
     }
+    // 去重
+    legendData = [...new Set(legendData)]
     return legendData;
 }
 
