22236d4eb52a61c428fe0556428537f410c2a39b
ader-h <1059803085@qq.com>
2025-04-21 18:09:32 -0700

    fix(Theme): modify interface compatibility

--- src/theme.js
+++ src/theme.js
@@ -13,7 +13,7 @@ import Token from './feature/token';
 import { THEMES } from './feature/token';
 import cloneDeep from './util/cloneDeep';
 
-window.getConsoleContext?.().get?.({ name: 'theme' }).getThemeFlagAsync?.().then((theme) => {
+window.getConsoleContext?.().get?.({ name: 'theme' })?.getThemeFlagAsync?.().then((theme) => {
   const cftheme = theme === 'dark' ? 'cloud-dark' : 'cloud-light';
   Theme.init(cftheme);
 })
