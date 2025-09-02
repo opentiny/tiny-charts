b1b138d1d43582aa030a2076f5369e7f2de30a87
ader-h <1059803085@qq.com>
2025-09-02 11:42:08 -0700

    feat(PieChart): add legend adaptive

--- src/option/config/legend/index.js
+++ src/option/config/legend/index.js
@@ -17,8 +17,12 @@ import textStyle from './textStyle';
 import itemStyle from './itemStyle';
 import merge from '../../../util/merge';
 import setPolymorphism from './polymorphism';
+import xkey from '../xAxis/xkey';
+import ldata from './ldata';
+import { updateLegendOccupancy } from './calculate';
+import { isArray } from '../../../util/type';
 
-function legend(iChartOption, chartName) {
+function legend(iChartOption, chartName, chartInstance) {
   const selfLegend = iChartOption.legend;
   const {
     data,
@@ -61,6 +65,16 @@ function legend(iChartOption, chartName) {
   // 图例多形态
   if( legend.orient === 'vertical' ){
     setPolymorphism(legend, iChartOption)
+  }
+  // 开启图例自适应的图表
+  const legendAdaptiveCharts = ['PieChart']; 
+  if (legendAdaptiveCharts.includes(chartName) && iChartOption.adaptive && legend.orient === 'vertical'){
+    const dataArr = isArray(iChartOption.data) ? iChartOption.data : [];
+    const xAxisKey = xkey(iChartOption);
+    const lData = ldata(dataArr, xAxisKey) || [];
+    const key = isArray(lData) ? lData[0] : undefined;
+    const legendData = key ? dataArr.map((item) => item?.[key])|| [] : [];
+    updateLegendOccupancy(iChartOption, legend, legendData, chartInstance);
   }
   return legend;
 }
