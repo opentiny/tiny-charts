2b93c8c1f5d8461f4000b345d3fce21bfbba3cd2
ader-h <1059803085@qq.com>
2025-09-29 17:28:05 -0700

    fix(Tooltip): modify comments

--- src/option/config/tooltip/formatter.js
+++ src/option/config/tooltip/formatter.js
@@ -17,7 +17,7 @@ function formatValue(value) {
     return value
 }
 
-// 移动端tolitip关闭按钮
+// 移动端tooltip关闭按钮
 function getCloseIcon(tooltipCloseColor = '#191919') {
    return `<svg width="16px" height="16px" viewBox="0 0 16 16" fill="none" customFrame="#000000" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
       <rect id="close" width="16" height="16" x="0" y="0"/>
